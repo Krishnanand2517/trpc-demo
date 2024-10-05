ed5d1591f38a218954bebc4f3654c51f66f84236
Krishnanand Yadav <krishnanand2517@gmail.com>
2024-10-05 19:25:50 +0530

    feat: Set up tRPC for the server

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,29 @@
 import express from "express";
+import { initTRPC } from "@trpc/server";
+import { createExpressMiddleware } from "@trpc/server/adapters/express";
+
+const t = initTRPC.create();
+
+const appRouter = t.router({
+  sayHi: t.procedure.query(() => {
+    return "Hi";
+  }),
+  logToServer: t.procedure
+    .input((v) => {
+      if (typeof v === "string") return v;
+
+      throw new Error("Invalid input: Expected string");
+    })
+    .mutation((req) => {
+      console.log(`Client says: ${req.input}`);
+
+      return true;
+    }),
+});
 
 const app = express();
+
+app.use("/trpc", createExpressMiddleware({ router: appRouter }));
 
 const PORT = 3000;
 
