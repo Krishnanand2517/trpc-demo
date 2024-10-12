fb8c88b67963772ed605c5ecf2a8f59cb28484a5
Krishnanand Yadav <krishnanand2517@gmail.com>
2024-10-13 00:42:40 +0530

    feat: Create and listen to websocket server in client

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
+import {
+  createTRPCProxyClient,
+  createWSClient,
+  httpBatchLink,
+  wsLink,
+  splitLink,
+} from "@trpc/client";
 import { AppRouter } from "../../server";
 
 createRoot(document.getElementById("root")!).render(
@@ -12,8 +18,22 @@ createRoot(document.getElementById("root")!).render(
   </StrictMode>
 );
 
+const wsClient = createWSClient({ url: "ws://localhost:3000/trpc" });
+
 const client = createTRPCProxyClient<AppRouter>({
-  links: [httpBatchLink({ url: "http://localhost:3000/trpc" })],
+  links: [
+    splitLink({
+      condition: (op) => {
+        return op.type === "subscription";
+      },
+      true: wsLink({ client: wsClient }),
+      false: httpBatchLink({ url: "http://localhost:3000/trpc" }),
+    }),
+  ],
+});
+
+document.addEventListener("click", () => {
+  client.users.updateUser.mutate({ userId: "23", name: "Krish New" });
 });
 
 async function main() {
@@ -26,14 +46,20 @@ async function main() {
   const result3 = await client.users.getUser.query({ userId: "23" });
   console.log(result3);
 
-  const result4 = await client.users.updateUser.mutate({
-    userId: "23",
-    name: "Krish",
-  });
-  console.log(result4);
+  // const result4 = await client.users.updateUser.mutate({
+  //   userId: "23",
+  //   name: "Krish",
+  // });
+  // console.log(result4);
 
   const result5 = await client.secretData.query();
   console.log(result5);
+
+  client.users.onUpdate.subscribe(undefined, {
+    onData: (id) => {
+      console.log(`Updated ${id}`);
+    },
+  });
 }
 
 main();
